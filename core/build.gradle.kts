f4ae6398cafc949f4929c1bbd643421cf1c59262
carrot <149851+lopcode@users.noreply.github.com>
2025-03-08 12:45:15 +0000

    Set up project

---
+++ core/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+
+    testLogging {
+        events = TestLogEvent.values().toSet() - TestLogEvent.STARTED
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
